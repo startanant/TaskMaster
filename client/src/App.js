36b845198180f0d96d09c6ec6d5db97219caf324
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-04-12 08:42:54 -0400

    updating App.js with imports for component, adding MainPage component for rendering

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Draggable from './components/Draggable/Draggable';
+import Droppable from './components/Droppable/Droppable';
+import Card from './components/Card/Card';
+import Column from './components/Column/Column';
+import MainPage from './components/MainPage/MainPage';
+function App() {
+    const [cards, addCard] = useState(0);
+    const Wrapper = styled.div`
+        width: 80%;
+        padding: 32px;
+        display: flex;
+        justify-content: center;
+    `;
+
+    return (
+        <Wrapper>
+            <MainPage id="qaz" />
+            
+        </Wrapper>
+    );
+}
+
+export default App;
