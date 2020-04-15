6af1d5c380defe51f3052ebc288551e234acbbe3
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-15 10:43:21 -0400

    added mailjet for sending emails throuh api, added shared model to mongoose

--- models/index.js
+++ models/index.js
@@ -22,7 +22,12 @@ const userAuthSchema = new mongoose.Schema(
     {},
     { collection: 'userauth', strict: false }
 );
+const sharedSchema = new mongoose.Schema(
+    { sharedFrom: String, sharedTo: String, sharedDashboards: Array },
+    { collection: 'shared' }
+);
 db.userprofile = mongoose.model('userprofile', userProfileSchema);
 db.userauth = mongoose.model('userauth', userAuthSchema);
+db.shared = mongoose.model('shared', sharedSchema);
 
 module.exports = db;
