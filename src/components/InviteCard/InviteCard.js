914d052b616a7d24d5ce775da60929ea1745e964
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-16 16:27:40 -0400

    added function to remove user from shared board

--- src/components/InviteCard/InviteCard.js
+++ src/components/InviteCard/InviteCard.js
@@ -1,8 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 function InviteCard(props) {
     const [email, setEmail] = useState('');
+    const sharedDashboards = props.sharedByUser ? props.sharedByUser : [];
     function handleInputChange(e) {
         setEmail(e.target.value);
+    }
+
+    function RenderList(props) {
+        const select = useRef(null);
+        if (!props.render) {
+            return <div>You are not sharing any dashboards</div>;
+        }
+        return (
+            <div>
+                You are sharing your dashbords<br></br>
+                <select ref={select}>
+                    <option></option>
+                    {sharedDashboards.map((dash) => {
+                        return (
+                            <>
+                                <option>{dash.to}</option>
+                            </>
+                        );
+                    })}
+                </select>
+                <button
+                    onClick={() => props.uninviteUser(select.current.value)}
+                >
+                    Del
+                </button>
+            </div>
+        );
     }
     return (
         <div style={{ margin: '10px' }}>
@@ -22,6 +50,12 @@ function InviteCard(props) {
             >
                 Invite
             </button>
+            <br></br>
+            {sharedDashboards.length > 0 ? (
+                <RenderList uninviteUser={props.uninviteUser} render={true} />
+            ) : (
+                <RenderList render={false} />
+            )}
         </div>
     );
 }
