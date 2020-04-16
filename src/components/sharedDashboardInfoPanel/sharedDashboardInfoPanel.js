e1ed52627be4aedf0e04ca3978d8ed182d7ac8ee
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-16 13:34:55 -0400

    changed name of component to start with capital letter because of some react issue

--- src/components/sharedDashboardInfoPanel/sharedDashboardInfoPanel.js
+++ src/components/sharedDashboardInfoPanel/sharedDashboardInfoPanel.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-function sharedDashboardInfoPanel(props) {
+function SharedDashboardInfoPanel(props) {
     const sharedDashboards = props.sharedDashboards;
-    function showDashboard() {}
+    const [show, setShow] = useState(false);
+    const select = useRef(null);
+
+    function showDashboard() {
+        select.current.value !== '' ? setShow(true) : setShow(false);
+    }
     return (
         <div>
             Shared Dahsboards<br></br>
             You have {sharedDashboards.length} dashboards shared with you!
             <br></br>
-            <select onChange={showDashboard}>
+            <select ref={select} onChange={showDashboard}>
                 <option></option>
                 {sharedDashboards.map((element) => {
                     return <option>{element.email}</option>;
                 })}
             </select>
+            <hr></hr>
+            <div>{show ? 'Dashboard to show if true' : ''}</div>
         </div>
     );
 }
 
-export default sharedDashboardInfoPanel;
+export default SharedDashboardInfoPanel;
