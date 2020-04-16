4ea4ca9d42c4695710007db97e8a6e48e8bad662
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-16 13:15:49 -0400

    user can see who shared dashboard with him in form of dropdown list

--- src/components/sharedDashboardInfoPanel/sharedDashboardInfoPanel.js
+++ src/components/sharedDashboardInfoPanel/sharedDashboardInfoPanel.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function sharedDashboardInfoPanel(props) {
-    return <div>Shared Dahsboards</div>;
+    const sharedDashboards = props.sharedDashboards;
+    function showDashboard() {}
+    return (
+        <div>
+            Shared Dahsboards<br></br>
+            You have {sharedDashboards.length} dashboards shared with you!
+            <br></br>
+            <select onChange={showDashboard}>
+                <option></option>
+                {sharedDashboards.map((element) => {
+                    return <option>{element.email}</option>;
+                })}
+            </select>
+        </div>
+    );
 }
 
 export default sharedDashboardInfoPanel;
