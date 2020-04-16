b9fb21946c41f0281bab3c0bacbf21d887d9b1b0
James Calverley <james.calverley3@gmail.com>
2020-04-15 20:17:15 -0400

    adds Header and SideNav components

---
+++ src/components/SideNav/SideNav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import './components-style.css';
+import { Link } from "react-router-dom";
+
+function SideNav(){
+  return (
+    <div className="sideNav">
+      <div className="icon-project-dashboard">
+        <Link to="/projectdashboard">Project</Link>
+      </div>
+      <div className="icon-mytasks">
+        <Link to="/mytasks" className="sidebar-icon">My Tasks</Link>
+      </div>
+      <div className="icon-settings">
+        <Link to="/settings" className="sidebar-icon">Settings</Link>
+      </div>
+    </div>
+  );
+}; 
+
+export default SideNav;
