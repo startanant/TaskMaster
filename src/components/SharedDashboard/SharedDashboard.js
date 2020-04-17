1d6c093055c0aaf364843ed05c7e96f6b40ac0d2
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-16 21:05:38 -0400

    update to SharedDashboard component to display available dashboards

--- src/components/SharedDashboard/SharedDashboard.js
+++ src/components/SharedDashboard/SharedDashboard.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 function SharedDashboard(props) {
+    const [show, setShow] = useState(false);
+    const dashboardsObj = props.toRender;
+    const indexesToRender = dashboardsObj.sharedByUser[0].dashboards;
+    const dashboards = dashboardsObj.dashboards;
+    function RenderSingleDashboard(props, el) {
+        if (!props.show) {
+            return null;
+        }
+        return <div>Dash will go here</div>;
+    }
     if (!props.render) {
         return null;
     }
-    return <div></div>;
+    return (
+        <div>
+            Click to see dashboards:
+            <br></br>
+            {indexesToRender.map((el) => {
+                return (
+                    <>
+                        <button
+                            onClick={() => {
+                                setShow(!show);
+                                RenderSingleDashboard(el);
+                            }}
+                        >
+                            Dash:{el}
+                        </button>
+                    </>
+                );
+            })}
+            <RenderSingleDashboard show={show} />
+        </div>
+    );
 }
 export default SharedDashboard;
