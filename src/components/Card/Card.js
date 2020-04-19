3dc6aa0155fa9659b3823ac0f221e66eac5b11c6
James Calverley <james.calverley3@gmail.com>
2020-04-19 17:22:33 -0400

    adds basic accordion functionality to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -51,6 +51,53 @@ function Card(props) {
                 </div>
             </div>
 
+            <div id={"accordion" + props.cardid}>
+                <div className="card">
+                <div className="card-utility-header">
+                    <button
+                        type="button"
+                        className="btn-sm btn-outline-secondary cardDelBtn"
+                        onClick={() =>
+                            props.deleteCard(props.colIndex, props.cardIndex)
+                        }
+                    >
+                        <i class="far fa-trash-alt"></i>
+                    </button>
+                </div>
+                <div className="card-header" id={"card-header" + props.cardid}>
+                    <TextArea
+                        id={'title' + props.cardid}
+                        value={props.title}
+                        placeholder="Title"
+                    />
+                     <button className="btn btn-link" data-toggle="collapse" data-target={"#collapseBody" + props.cardid} aria-expanded="true" aria-controls={"collapseBody" + props.cardid}>
+                        more
+                    </button>
+                </div>
+                <div id={"collapseBody" + props.cardid} className="collapse" aria-labelledby={"card-header" + props.cardid} data-parent={"#accordion" + props.cardid}>
+                    <div className="card-body">
+                        {/* <TextArea
+                            id={'desc' + props.cardid}
+                            placeholder="Description"
+                            value={props.description}
+                        /> */}
+                        <TextAreaDesc 
+                            id={'desc' + props.cardid}
+                            placeholder="Description"
+                            value={props.description}
+                        />
+                        <DueDate id={'date' + props.cardid} value={props.dueDate} />
+                        <AssignCard
+                            id={'email' + props.cardid}
+                            shared={props.shared}
+                            colIndex={props.colIndex}
+                            cardIndex={props.cardIndex}
+                            assignToCard={props.assignToCard}
+                        />
+                    </div>
+                </div>
+            </div>
+            </div>
             {/* <div style={style} id={props.cardid}> */}
             {/* <textarea
                 id={'title' + props.cardid}
