e0ce2126211a0c62da28bea6670763a4ecae9d0a
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-20 11:22:39 -0400

    timer reduction for login from 2s to 1.5 s

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -21,7 +21,7 @@ import Message from '../Message/Message';
 //     );
 // };
 
-function LoginPage (props) {
+function LoginPage(props) {
     // const isLoggedIn = localStorage.getItem('email') ? true : false;
     const [globalData, dispatch] = useGlobalStore();
     const [userData, setUserData] = useState({ email: '', password: '' });
@@ -121,9 +121,9 @@ function LoginPage (props) {
                 message: 'Logging in...',
             });
             setTimeout(function () {
-                dispatch({ do: 'clearMessage'});
+                dispatch({ do: 'clearMessage' });
                 dispatch({ do: 'loginState', loggedIn: true });
-            }, 2000);
+            }, 1500);
         }
 
         // setTimeout(function () {
@@ -137,66 +137,65 @@ function LoginPage (props) {
     }
 
     return (
-       
-            
         <div className="loginPage">
             {globalData.loggedIn ? <Redirect to="/projectdashboard" /> : ''}
-                
-                <div className="card login-card">
-                    <div className="card-header title-header">
-                        <h1>TaskMaster</h1>
-                        <h4 style={{color: 'grey'}}>Keep your projects organized.</h4>
-                    
-                    </div>
-                    <div className="card-header">Login</div>
-                    <Message />
-                    <div className="card-body">
-                    
-                            <input type="hidden" id="db_id" value="" />
-                            <div className="form-group">
-                                <label for="email">Email</label>
-                                <input
-                                    value={userData.email}
-                                    onChange={handleInputChange}
-                                    ref={inputEmail}
-                                    id="email"
-                                    type="email"
-                                    className="form-control login-input"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label for="userPassword">Password</label>
-                                <input
-                                    value={userData.password}
-                                    onChange={handleInputChange}
-                                    ref={inputPassword}
-                                    id="password"
-                                    type="password"
-                                    className="form-control login-input "
-                                />
-                            </div>
-                            <button
-                                onClick={loginUser}
-                                type="button"
-                                className="btn btn-outline-light submit"
-                            >
-                                Login
-                            </button>
-                            <footer className="footer-card">
-                                Don't have an account?
-                                <Link to="/register" className="loginBtn">
-                                    <button type="button" className="btn btn-sm btn-outline-light">Register</button>
-                                </Link>
-                            </footer>
-                        
-                    </div>
-                </div>
 
-                {/* <button onClick={() => handleLogin()}>Click here to log in</button> */}
-            
-        
+            <div className="card login-card">
+                <div className="card-header title-header">
+                    <h1>TaskMaster</h1>
+                    <h4 style={{ color: 'grey' }}>
+                        Keep your projects organized.
+                    </h4>
+                </div>
+                <div className="card-header">Login</div>
+                <Message />
+                <div className="card-body">
+                    <input type="hidden" id="db_id" value="" />
+                    <div className="form-group">
+                        <label for="email">Email</label>
+                        <input
+                            value={userData.email}
+                            onChange={handleInputChange}
+                            ref={inputEmail}
+                            id="email"
+                            type="email"
+                            className="form-control login-input"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label for="userPassword">Password</label>
+                        <input
+                            value={userData.password}
+                            onChange={handleInputChange}
+                            ref={inputPassword}
+                            id="password"
+                            type="password"
+                            className="form-control login-input "
+                        />
+                    </div>
+                    <button
+                        onClick={loginUser}
+                        type="button"
+                        className="btn btn-outline-light submit"
+                    >
+                        Login
+                    </button>
+                    <footer className="footer-card">
+                        Don't have an account?
+                        <Link to="/register" className="loginBtn">
+                            <button
+                                type="button"
+                                className="btn btn-sm btn-outline-light"
+                            >
+                                Register
+                            </button>
+                        </Link>
+                    </footer>
+                </div>
+            </div>
+
+            {/* <button onClick={() => handleLogin()}>Click here to log in</button> */}
         </div>
-        
     );
 }
 
