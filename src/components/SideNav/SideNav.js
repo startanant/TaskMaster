71baba824cefac0bcbf703fab2e8cd64eade8cc9
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-20 18:12:56 -0400

    commenting out Setting link

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,63 +1,53 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import '../../components-style.css';
-import { Link } from "react-router-dom";
-import { useGlobalStore } from "../GlobalStore/GlobalStore";
+import { Link } from 'react-router-dom';
+import { useGlobalStore } from '../GlobalStore/GlobalStore';
 
-function SideNav(){
-  const [globalData, dispatch] = useGlobalStore();
-  // const [userLogout, setUserLogout] = useState(false);
+function SideNav() {
+    const [globalData, dispatch] = useGlobalStore();
+    // const [userLogout, setUserLogout] = useState(false);
 
-  function logout() {
-    dispatch({ do: 'loginState', loggedIn: false })
-    localStorage.removeItem('email');
-  }
+    function logout() {
+        dispatch({ do: 'loginState', loggedIn: false });
+        localStorage.removeItem('email');
+    }
 
-  return (
+    return (
+        <div className="sideNav">
+            {globalData.loggedIn ? '' : <Redirect to="/" />}
+            <div className="taskmaster-logo">TaskMaster</div>
 
-    <div className="sideNav">
+            <Link to="/projectdashboard" className="nav-item">
+                <div className="nav-icon">
+                    <i class="fas fa-columns"></i>
+                </div>
+                <div className="nav-heading">Dashboards</div>
+            </Link>
 
-      {globalData.loggedIn ? '' : <Redirect to='/' />}
-      <div className="taskmaster-logo">
-          TaskMaster
-        </div>
-      
+            <Link to="/mytasks" className="nav-item">
+                <div className="nav-icon">
+                    <i className="fas fa-list"></i>
+                </div>
+                <div className="nav-heading">My Tasks</div>
+            </Link>
 
-      <Link to="/projectdashboard" className="nav-item">
-          <div className="nav-icon">
-            <i class="fas fa-columns"></i>
-          </div>
-          <div className="nav-heading">
-            Dashboards
-          </div>
-      </Link>
-
-      <Link to="/mytasks" className="nav-item">
-          <div className="nav-icon">
-            <i className="fas fa-list"></i>
-          </div>
-          <div className="nav-heading">
-            My Tasks
-          </div>
-      </Link>
-
-      <Link to="/settings" className="nav-item">
+            {/* <Link to="/settings" className="nav-item">
           <div className="nav-icon">
             <i class="fas fa-cog"></i>
           </div>
           <div className="nav-heading">
             Settings
           </div>
-      </Link>
+      </Link> */}
 
-      <Link onClick={logout} className="nav-item">
-        <div className="nav-icon">
-          <i class="fas fa-door-open"></i>
+            <Link onClick={logout} className="nav-item">
+                <div className="nav-icon">
+                    <i class="fas fa-door-open"></i>
+                </div>
+                <div className="nav-heading">Logout</div>
+            </Link>
         </div>
-        <div className="nav-heading">Logout</div>
-      </Link>
-      
-    </div>
     );
 }
 
