1c060feea9a6e823864ab48753460e9086d5d088
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-24 13:40:20 -0400

    Login function moved from LoginPage to App, removed unused code

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -5,22 +5,6 @@ import { login } from '../../utils';
 import { Link } from 'react-router-dom';
 import Message from '../Message/Message';
 
-// const LoginPage = (props) => {
-
-//     const handleLogin = () => {
-//         login();
-//         props.history.push('/projectdashboard');
-//     }
-
-//     return (
-//         <div>
-//             <h1>Sign in</h1>
-
-//             <button onClick={() => handleLogin()}>Click here to log in</button>
-//         </div>
-//     );
-// };
-
 function LoginPage(props) {
     // const isLoggedIn = localStorage.getItem('email') ? true : false;
     const [globalData, dispatch] = useGlobalStore();
@@ -28,126 +12,18 @@ function LoginPage(props) {
 
     const inputEmail = useRef();
     const inputPassword = useRef();
-    if (localStorage.getItem('email')) {
-        dispatch({ do: 'loginState', loggedIn: true });
-    }
+    // if (localStorage.getItem('email')) {
+    //     dispatch({ do: 'loginState', loggedIn: true });
+    // }
 
     function handleInputChange(e) {
         const { id, value } = e.target; //
 
         setUserData({ ...userData, [id]: value });
     }
-
-    async function m_login() {
-        let testUser = {
-            email: 'tomcruise2@tom.com',
-            name: 'tommy',
-            firstname: 'Tom',
-            lastname: 'Cruise',
-            password: 'myOwnSecret@1',
-        };
-        const url = '/api/addUser';
-        const result = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(testUser),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then((response) => response.json());
-        console.log(result);
-    }
-
-    function handleLogin() {
-        //m_login();
-        login();
-        let email = 'justin@trudeau.com';
-        // props.history.push('/projectdashboard');
-        localStorage.setItem('email', email);
-        props.history.push({
-            pathname: '/projectdashboard',
-            state: { email: email },
-        });
-    }
-
-    async function loginUser(e) {
-        e.preventDefault();
-
-        if (userData.email === '') {
-            inputEmail.current.focus();
-            dispatch({
-                do: 'setMessage',
-                type: 'danger',
-                message: 'Please enter your email!',
-            });
-            //alert('Please enter your email!');
-            return;
-        }
-
-        if (userData.password === '' || userData.password.length < 1) {
-            inputPassword.current.focus();
-            dispatch({
-                do: 'setMessage',
-                type: 'danger',
-                message: 'Please enter your password!',
-            });
-            //alert('Please enter your password!');
-            return;
-        }
-
-        //const apiResult = await API.post('/api/user/login', userData);
-
-        // if (apiResult.error) {
-        //     dispatch({ do: 'setMessage', type: 'danger', message: apiResult.error });
-        //     // clear any session
-        //     localStorage.session = '';
-        //     return;
-        // };
-
-        //loginComplete(apiResult);
-        console.log(userData);
-        const url = '/api/login';
-        const result = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(userData),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then((response) => response.json());
-        console.log(result);
-        // console.log(result[0].email);
-        //let email = result.email;
-        if (result.email) {
-            localStorage.setItem('email', result.email);
-            dispatch({
-                do: 'setMessage',
-                type: 'success',
-                message: 'Logging in...',
-            });
-            setTimeout(function () {
-                dispatch({ do: 'clearMessage' });
-                dispatch({ do: 'loginState', loggedIn: true });
-            }, 1000);
-        } else {
-            dispatch({
-                do: 'setMessage',
-                type: 'danger',
-                message: result.message
-            });
-        }
-
-        // setTimeout(function () {
-        //     dispatch({ do: 'setMessage', type: 'success', message: 'Logging in...' });
-        //     dispatch({ do: 'loginState', loggedIn: true })
-        // }, 2000);
-        // props.history.push({
-        //     pathname: '/projectdashboard',
-        //     state: { email: email }
-        // });
-    }
-
     return (
         <div className="loginPage">
-            {globalData.loggedIn ? <Redirect to="/projectdashboard" /> : ''}
+            {/* {globalData.loggedIn ? <Redirect to="/" /> : ''} */}
 
             <div className="card login-card">
                 <div className="card-header title-header">
@@ -183,7 +59,7 @@ function LoginPage(props) {
                         />
                     </div>
                     <button
-                        onClick={loginUser}
+                        onClick={() => props.login(userData)}
                         type="button"
                         className="btn btn-outline-light submit"
                     >
