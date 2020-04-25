78169d5f827aceb0c1e71ac103a6135068c47480
KiteRunner2 <36109470+KiteRunner2@users.noreply.github.com>
2020-04-25 15:01:24 -0400

    updates to chat component

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8080');
 
 function Chat() {
     const [value, setValue] = useState('');
-
-    useEffect(() => {
-        socket.on('update', (msg) => {
-            // document.getElementById('m').value = msg;
-            setValue(msg);
-        });
-    }, []);
+    // const socket = openSocket('http://localhost:8080');
+    // socket.on('update', (msg) => {
+    //     // document.getElementById('m').value = msg;
+    //     setValue('s:' + socket.id + 'm:' + msg);
+    // });
     return (
         <div>
-            <p>{value}aaa</p>
+            <p style={{ color: 'white' }}>{value}aaa</p>
         </div>
     );
 }
