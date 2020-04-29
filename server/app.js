7f544943338139e9b5b6c756370efa729e9a3869
Anant S <startanant@gmail.com>
2020-04-28 23:51:49 -0400

    Updates session in app.js

--- server/app.js
+++ server/app.js
@@ -14,10 +14,133 @@ const bcrypt = require('bcrypt');
 // import { v4 as uuidv4 } from 'uuid';
 let db = require('./models');
 let user = require('./user.json');
+const API_URL = process.env.MLAB
+    ? 'https://taskmaster0.herokuapp.com' : 'http://localhost:8080';
 // let sharedDashboard = require('./shared.json');
 
 // const db_host = process.env.DB_HOST;
 // console.log(process.env);
+
+// session checking middleware
+async function needSession(req, res, next) {
+    console.log(`[middleware] session url(${req.url}) session(${req.headers.session || ''}) `);
+
+    // check session set, and it's valid
+    if (!req.headers.session ||
+        req.headers.session.length !== 36 ||
+        !(await checkSession(req.headers.session))) {
+
+        console.log('[middleware:session] invalid session, indicating redirect');
+        res.status(403).send({ error: 'Requires valid session. Please login again.' });
+        return;
+    }
+
+    // session was good, let's continue endpoint processing...
+    next();
+}
+
+// input: session
+// output: boolean
+async function checkSession(session) {
+    const userData = await db.userprofile.findOne({ session });
+    console.log(`[checkSession] session(${session}) -> valid? ${userData._id ? true : false}`);
+    return (userData._id ? true : false);
+}
+
+// OAUTH Authentication
+async function createOAuthSession(userData) {
+    console.log('[createOAuthSession]', userData);
+
+    // register user in system (if they aren't there, and get the associated session)
+    const session = uuid.v4();
+    const authUserData = await registerUser(userData, session);
+
+    // returns the logged-in user info to javascript
+    return authUserData;
+}
+// oAuth - list providers we'll accept .env info for
+require('./oAuth')(app, API_URL, ['twitter', 'google', 'facebook', 'github'], createOAuthSession);
+
+// if we give a 'session', it will save that for this newly registered user
+// OR if the user exists, it will UPDATE the session
+// input: <object> { name, email, password }, session
+// output: { message, id, name }
+async function registerUser(userData, session = '') {
+    if (!userData.name || (userData.type === 'local' && !userData.email)) {
+        console.log('[registerUser] invalid userData! ', userData);
+        return { message: 'Invalid user data', id: '', name: '' };
+    }
+
+    let passwordHash = '';
+    if (!userData.type || userData.type === 'local') {
+        if (!userData.password) {
+            console.log('[registerUser] invalid userData (need password)! ', userData);
+            return { message: 'Invalid user password', id: '', name: '' };
+        }
+        const saltRounds = 10;
+        passwordHash = await bcrypt.hash(userData.password, saltRounds);
+        console.log(`[registerUser] (hash=${passwordHash}) req.body:`, userData);
+        userData.type = 'local';
+    }
+
+    console.log('[registerUser], userData: ', userData);
+
+
+
+    // check if user exists, and refuse for local users, and quietly change just session for other types
+    let duplicateUser = {};
+    if (!userData.authId) {
+        duplicateUser = await db.userprofile.findOne({ email: userData.email });
+
+        if (duplicateUser && duplicateUser._id) {
+            return {
+                error: 'Duplicate email, try another or login',
+                id: false, session: false
+            };
+        }
+    } else {
+        duplicateUser = await db.userprofile.findOne({ authId: userData.authId });
+
+        if (duplicateUser && duplicateUser._id) {
+            const saveUser = await db.userprofile.findByIdAndUpdate({ _id: duplicateUser._id }, { session });
+            console.log(`   -> duplicate user (ie they've logged in before via oAuth), just update session: ${session}`, saveUser);
+            return {
+                message: `Welcome back ${saveUser.name}`,
+                id: saveUser._id,
+                name: saveUser.name,
+                email: saveUser.email,
+                thumbnail: saveUser.thumbnail,
+                session,
+                createdAt: saveUser.createdAt
+            };
+        }
+    }
+
+
+    const saveData = {
+        name: userData.name,
+        email: userData.email || '',
+        thumbnail: userData.thumbnail || '',
+        authId: userData.authId || '',
+        password: passwordHash,
+        type: userData.type,
+        session
+    };
+
+    const dbUser = new db.userprofile(saveData);
+    const saveUser = await dbUser.save();
+    return {
+        message: `Success! ${saveUser.name} was successfully registered`,
+        id: saveUser._id,
+        name: saveUser.name,
+        email: saveUser.email,
+        thumbnail: saveUser.thumbnail,
+        session,
+        createdAt: saveUser.createdAt
+    };
+}
+
+
 
 async function getUsers() {
     const result = await db.find({}, { email: 1 });
@@ -152,6 +275,7 @@ app.post('/api/addUser', async (req, res) => {
     passwordHash = await bcrypt.hash(req.body.password, saltRounds);
     console.log(`[addUser] (hash=${passwordHash}) req.body:`, user);
     user.password = passwordHash;
+    user.session = null;
 
     try {
         const response = await db.userprofile.create(user);
